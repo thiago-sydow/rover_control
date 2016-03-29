d75027dab66036dbcf0894502d9af0db3b765fff
Thiago von Sydow <thiagovs05@gmail.com>
2016-03-29 20:21:42 -0300

    added desired output scenario and boundaries error scenario

--- spec/rover_control_spec.rb
+++ spec/rover_control_spec.rb
@@ -5,7 +5,18 @@ describe RoverControl do
     expect(RoverControl::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context 'with valid boundaries' do
+    it 'receives and processes the commands' do
+      str = "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM"
+      expect(RoverControl.process_rovers_instructions(str)).to eq "1 3 N\n5 1 E"
+    end
   end
+
+  context 'with invalid boundaries' do
+    it 'receives and processes the commands' do
+      str = "5 \n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM"
+      expect { RoverControl.process_rovers_instructions(str) }.to raise_error(ArgumentError)
+    end
+  end
+
 end
