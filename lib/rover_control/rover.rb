803f8b4db124cf5b09744b97597160aab1c4f56f
Thiago von Sydow <thiagovs05@gmail.com>
2016-03-29 21:02:51 -0300

    added process_commands method returning the position after executed

--- lib/rover_control/rover.rb
+++ lib/rover_control/rover.rb
@@ -5,6 +5,23 @@ module RoverControl
       @position = PositionController.new(rover_position.split(' '), x_limit, y_limit)
     end
 
-  end
+    def process_commands(commands)
+      commands.chars.each do |command|
+        if command == 'M'
+          @position.move
+        elsif %w(R L).include?(command)
+          @position.turn(command)
+        else
+          raise InvalidCommandError
+        end
+      end
 
+      current_position
+    end
+
+    def current_position
+      @position.to_s
+    end
+
+  end
 end
