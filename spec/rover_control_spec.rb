d7cbe3a1a53a59e4b81608953c1c34237f28f87e
Thiago von Sydow <thiagovs05@gmail.com>
2016-03-29 22:32:20 -0300

    scenario for put different ranges os rovers and commands

--- spec/rover_control_spec.rb
+++ spec/rover_control_spec.rb
@@ -19,4 +19,17 @@ describe RoverControl do
     end
   end
 
+  context 'a lot of commands' do
+    examples = [
+      ["5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM", "1 3 N\n5 1 E"],
+      ["3 3\n1 2 S\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM", "1 1 S\nerror"],
+      ["3 4\n1 3 E\nLMLLLMLML\n3 2 S\nMLRMRRMLRM", "error\n3 2 N"],
+      ["2 2\n2 2 W\nLMR", "2 1 W"],
+      ["8 8\n5 6 W\nLMLRL\n2 4 E\nRMRML\n1 5 S\nRLMMRRRLRL\n0 0 N\nLRRMMRMMRR", "5 5 E\n1 3 S\n1 3 N\nerror"],
+    ]
+    examples.each do |input, output|
+      it { expect(RoverControl.process_rovers_instructions(input)).to eq output }
+    end
+  end
+
 end
